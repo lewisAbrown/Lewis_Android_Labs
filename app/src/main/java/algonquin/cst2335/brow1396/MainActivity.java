d730440574bf388697d6d50dfa22096c84ecb17f
lewis <brow1396@algonquinlive.com>
2022-09-19 17:41:55 -0400

    Lab 2 part 2/6 (onclick working)

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.brow1396;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -17,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         TextView firstText = findViewById(R.id.firstString); // either returns a text view or a null if firstString not on the page
         firstText.setText("Java changed this");
 
-    }
-}
+        EditText second = findViewById(R.id.secondString);
+        second.setText("I'm an edit text"); //Java change text at launch
+
+        String holder = "Your edit text has:"; //Place holder for onclick
+        Button button = findViewById(R.id.button); //link button to java
+
+        // event listener for when the button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String Return = second.getText().toString(); //set variable return to hold the new input in text box
+                second.setText(holder + Return); //set second text box to variable holder and return
+            }
+        });
+}}
