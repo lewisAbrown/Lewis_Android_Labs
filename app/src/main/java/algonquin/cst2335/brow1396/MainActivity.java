1861f52c80700dd8c557313a4f5adc41b35dabfc
lewis <brow1396@algonquinlive.com>
2022-09-19 18:25:16 -0400

    Lab 2 part 2/6 Complete with Lambda

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -1,13 +1,7 @@
 package algonquin.cst2335.brow1396;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import algonquin.cst2335.brow1396.databinding.ActivityMainBinding;
 
