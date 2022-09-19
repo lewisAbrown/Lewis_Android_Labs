9ae68b3c5f745bc75ad06143f2d46051ee4c4321
lewis <brow1396@algonquinlive.com>
2022-09-19 16:33:54 -0400

    Lab 2 part 2/6 (half way through)

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -3,6 +3,8 @@ package algonquin.cst2335.brow1396;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load the first text view
+        TextView firstText = findViewById(R.id.firstString); // either returns a text view or a null if firstString not on the page
+        firstText.setText("Java changed this");
+
     }
 }
