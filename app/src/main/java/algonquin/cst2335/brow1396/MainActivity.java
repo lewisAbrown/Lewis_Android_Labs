7d83509c361640e0bbb8a5522fdf0e04f51bd332
lewis <brow1396@algonquinlive.com>
2022-09-19 18:16:37 -0400

    Lab 2 part 2/6 Complete with Lambda

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -2,35 +2,36 @@ package algonquin.cst2335.brow1396;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.brow1396.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        //load the first text view
-        TextView firstText = findViewById(R.id.firstString); // either returns a text view or a null if firstString not on the page
-        firstText.setText("Java changed this");
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater()); // this will load my pre-made variable from ViewBinding
 
-        EditText second = findViewById(R.id.secondString);
-        second.setText("I'm an edit text"); //Java change text at launch
+        variableBinding.firstString.setText("Java changed this");
 
-        String holder = "Your edit text has:"; //Place holder for onclick
-        Button button = findViewById(R.id.button); //link button to java
+        variableBinding.secondString.setText("Im an edit text");
+
+        setContentView(variableBinding.getRoot()); //this shows the objects on screen
 
         // event listener for when the button is clicked
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String Return = second.getText().toString(); //set variable return to hold the new input in text box
-                second.setText(holder + Return); //set second text box to variable holder and return
-            }
+        variableBinding.button.setOnClickListener( (view) -> {
+            String Return = variableBinding.secondString.getText().toString(); //set variable return to hold the new input in text box
+            variableBinding.secondString.setText("Your edit text has: " + Return); //set second text box to variable holder and return
         });
+
+
 }}
