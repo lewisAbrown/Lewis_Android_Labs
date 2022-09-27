a77275e388a835fe849be28fc4d687c0d049bfa2
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-09-27 12:54:15 -0400

    Week 3 set 1 complete

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -1,14 +1,17 @@
 package algonquin.cst2335.brow1396;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import algonquin.cst2335.brow1396.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(variableBinding.getRoot());
+
     }
 }
