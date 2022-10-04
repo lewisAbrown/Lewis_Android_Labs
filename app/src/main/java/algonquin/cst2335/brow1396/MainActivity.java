f60f269c2a7087a95ee9f9cf52dd9ef05099e07c
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-10-04 12:32:33 -0400

    week 4 with Activity second (buttons added)

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -1,6 +1,7 @@
 package algonquin.cst2335.brow1396;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,29 +10,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Loads XML file
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
     }
 
+    //activity is visible but not responding to touch
     @Override
     protected void onStart() {
         super.onStart();
     }
 
+    //activity is now visible and responds to user input
     @Override
     protected void onResume() {
         super.onResume();
     }
 
+    //activity is visible but not responding to touch
     @Override
     protected void onPause() {
         super.onPause();
     }
 
+    //activity is no longer visible
     @Override
     protected void onStop() {
         super.onStop();
     }
 
+    //the garbage collector is now clearing memory
     @Override
     protected void onDestroy() {
         super.onDestroy();
---
+++ app/src/main/java/algonquin/cst2335/brow1396/SecondActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.brow1396;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
