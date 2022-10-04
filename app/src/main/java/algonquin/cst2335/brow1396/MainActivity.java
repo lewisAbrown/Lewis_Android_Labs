31b23ea6b7ecf89edceefb3f162cd5a0ef04b0c4
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-10-04 11:48:41 -0400

    week 4 start

--- app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/MainActivity.java
@@ -1,8 +1,8 @@
 package algonquin.cst2335.brow1396;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
