9fcc2983ed57c602c32bcf54876b0875cb49a368
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-10-05 15:43:52 -0400

    Week 4 part 1/2 (part 1 almost complete (not confident))

--- app/src/main/java/algonquin/cst2335/brow1396/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/SecondActivity.java
@@ -1,8 +1,15 @@
 package algonquin.cst2335.brow1396;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import algonquin.cst2335.brow1396.databinding.ActivitySecondBinding;
@@ -10,6 +17,8 @@ import algonquin.cst2335.brow1396.databinding.ActivitySecondBinding;
 public class SecondActivity extends AppCompatActivity {
 
     ActivitySecondBinding binding;
+    Bitmap mBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +33,23 @@ public class SecondActivity extends AppCompatActivity {
         binding.textView.setText("Welcome back " + emailAddress);
 
     }
+
+    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+    ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+
+                        Intent data = result.getData();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+                        binding.profileImage.setImageBitmap( thumbnail);
+                    }
+                }
+            });
+
+
+
 }
