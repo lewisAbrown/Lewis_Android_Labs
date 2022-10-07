a190c1203e674f2a2e53d85d4827510b38d10938
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-10-07 15:14:52 -0400

    Week 4 part 1/2 (phone working, camera sort of working)

--- app/src/main/java/algonquin/cst2335/brow1396/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/brow1396/SecondActivity.java
@@ -20,6 +20,22 @@ public class SecondActivity extends AppCompatActivity {
     ActivitySecondBinding binding;
     Bitmap mBitmap;
 
+    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+
+                        Intent data = result.getData();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+                        binding.profileImage.setImageBitmap(thumbnail);
+                        startActivity(data);
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,30 +51,12 @@ public class SecondActivity extends AppCompatActivity {
 
         binding.button.setOnClickListener(clk -> {
             Intent call = new Intent(Intent.ACTION_DIAL);
-            call.setData(Uri.parse("tel:" + call.putExtra("editTextPhone", binding.editTextPhone.getText())));
+            String phoneNumber = binding.editTextPhone.getText().toString();
+            call.setData(Uri.parse("tel:" + phoneNumber));
             startActivity(call);
         });
 
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        binding.buttonimage.setOnClickListener(clk->{
-        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-
-                            Intent data = result.getData();
-                            Bitmap thumbnail = data.getParcelableExtra("data");
-                            binding.profileImage.setImageBitmap(thumbnail);
-                            startActivity(data);
-                        }
-                    }
-                });
-
-        cameraResult.launch(cameraIntent);
-
-        });
+        binding.buttonimage.setOnClickListener(clk->
+        cameraResult.launch(cameraIntent));
 }
 }
