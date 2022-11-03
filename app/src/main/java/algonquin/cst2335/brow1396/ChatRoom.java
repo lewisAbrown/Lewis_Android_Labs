6517384bfe91f0a0bafc174b37781374d60aaeb8
lewisAbrown <113211452+lewisAbrown@users.noreply.github.com>
2022-11-03 16:00:42 -0400

    Week 6 part 2 almost complete (at LayoutManager step)

--- app/src/main/java/algonquin/cst2335/brow1396/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/brow1396/ChatRoom.java
@@ -3,22 +3,39 @@ package algonquin.cst2335.brow1396;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 import algonquin.cst2335.brow1396.databinding.ActivityChatRoomBinding;
+import algonquin.cst2335.brow1396.databinding.SentMessageBinding;
 
 public class ChatRoom extends AppCompatActivity {
 
+    /**
+     * inner Class MyRowHolder
+     */
     class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timeText;
+
         public MyRowHolder(@NonNull View itemView) {
             super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+            timeText = itemView.findViewById(R.id.timeText);
         }
     }
 
+    /**
+     * variables and constructor
+     */
     ActivityChatRoomBinding binding;
+    ArrayList<String> messages = new ArrayList<>();
+    private RecyclerView.Adapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +44,38 @@ public class ChatRoom extends AppCompatActivity {
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>(){
+        binding.sendButton.setOnClickListener(click -> {
+           messages.add(binding.textInput.getText().toString());
+           myAdapter.notifyItemInserted(messages.size()-1);
+           binding.textInput.setText("");
+        });
+
+        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>(){
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position){
-
+                holder.messageText.setText("");
+                holder.timeText.setText("");
+                String obj = messages.get(position);
+                holder.messageText.setText(obj);
             }
 
             @Override
             public int getItemCount(){
+                return messages.size();
+            }
+
+            @Override
+            public int getItemViewType(int position){
                 return 0;
             }
+
         } );
     }
 }
